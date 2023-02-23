0667af488bd4eb9ca434262f1a6abcdad53b4c88
Riku <riku.helastera@outlook.com>
2023-02-23 19:04:24 +0200

    movement t. andero, riku, elmeri

--- game_movement.py
+++ game_movement.py
@@ -1,21 +1,93 @@
 import mysql.connector
+import game_objects
 from geopy import distance
 
-
 connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='HTM_Database',
-         user='root',
-         password='Salasana1!',
-         autocommit=True
-         )
+        host='localhost',
+        database='htm_database',
+        user='htm',
+        password='play',
+        autocommit=True
+    )
 
-
-
-def select_airport():
+def select_airport(connection):
     sql = "SELECT iso_country, ident, name, latitude_deg, longitude_deg FROM airport WHERE continent = NA"
-    cursor = conn.cursor(Dictionary=True)
+    cursor = connection.cursor(Dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchone()
     return result
+
+def get_player_coordinates(player_location, connection):
+    sql = f'SELECT latitude_deg, longitude_deg FROM airport WHERE ident = \'{player_location}\''
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    tulos = cursor.fetchall()
+    return tulos[0]
+
+def get_all_airport_coordinates(connection):
+    sql = f'SELECT name, latitude_deg, longitude_deg, ident FROM airport'
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    airport_coordinates = []
+    for row in result:
+        airport_coordinates.append(row)
+    return airport_coordinates
+
+def calculate_distance(location_data, player_location):
+    player_coordinates = (player_location[0], player_location[1])
+    airport_location = (location_data[0], location_data[1])
+    return float(distance.distance(airport_location, player_coordinates).km)
+
+def calculate_all_airport_distance(airport_list, player_coordinates):
+    distances = []
+    for row in airport_list:
+        location_data = (row[1], row[2])
+        airport_distance = (row[0], calculate_distance(location_data ,player_coordinates))
+        distances.append(airport_distance)
+    return distances
+
+def airports_in_range(airport_list):
+    in_range = []
+    for row in airport_list:
+        if row[1] <= 500:
+            in_range.append(row)
+    return in_range
+
+
+def player_movement(in_range, connection):
+    i = 1
+    airport_dic = dict()
+
+    for row in in_range:
+        print(f'({i}) {row[0]}')
+        airport_dic.update({i:row[0]})
+        i+=1
+    answer = input(f'Choose your destination: ')
+
+    try:
+        if airport_dic.get(int(answer)):
+            pass
+    except:
+        print("\nInvalid value\n")
+        player_movement(in_range)
+    query = f'SELECT ident FROM airport WHERE name LIKE "{airport_dic.get(int(answer))}"'
+    
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    result = str(result).strip('[(,)]')
+    print(result)
+    placeholder_id = 'Player'
+
+    update = f'UPDATE game SET location = {result} WHERE id = \'{placeholder_id}\''
+    cursor.execute(update)
+
+
+
+
+
+
+calculated = calculate_all_airport_distance(get_all_airport_coordinates(connection), get_player_coordinates('KIND', connection))
+
+player_movement(airports_in_range(calculated), connection)
