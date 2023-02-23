755c863434ccc6ccb8b845cc14158425471653c9
AnderoMu <muurisepp.andero@gmail.com>
2023-02-23 14:02:05 +0200

    game.py -Select_airports

--- game.py
+++ game.py
@@ -12,18 +12,10 @@ connection = mysql.connector.connect(
          )
 
 
-# Select airports for the game.
+
 def select_airports():
     sql = "SELECT iso_country, ident, name, latitude_deg, longitude_deg FROM airport WHERE continent = NA"
     cursor = conn.cursor(Dictionary=True)
     cursor.execute(sql)
-    result = cursor.fetchone()
-    return result
-
-# Gets info about the airports.
-def airport_info(icao):
-    sql = "SELECT iso_country, ident, name, latitude_deg, longitude_deg FROM airport"
-    cursor = conn.cursor(Dictionary=True)
-    cursor.execute(sql, (icao,))
     result = cursor.fetchone()
     return result
