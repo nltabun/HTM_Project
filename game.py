324faf68482b200abb16c54d8f3c76f084425256
AnderoMu <muurisepp.andero@gmail.com>
2023-02-23 09:54:32 +0200

    game.py -Select_airports -Airport_info

--- game.py
+++ game.py
@@ -5,11 +5,25 @@ from geopy import distance
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
-         database='HTM',
+         database='HTM_Database',
          user='root',
-         password='',
+         password='Salasana1!',
          autocommit=True
          )
 
 
+# Select airports for the game.
+def select_airports():
+    sql = "SELECT iso_country, ident, name, latitude_deg, longitude_deg FROM airport WHERE continent = NA"
+    cursor = conn.cursor(Dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return result
 
+# Gets info about the airports.
+def airport_info(icao):
+    sql = "SELECT iso_country, ident, name, latitude_deg, longitude_deg FROM airport"
+    cursor = conn.cursor(Dictionary=True)
+    cursor.execute(sql, (icao,))
+    result = cursor.fetchone()
+    return result
