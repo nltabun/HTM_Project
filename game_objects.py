5f5f21214f62aae285f13987186b172c6478441e
Riku <riku.helastera@outlook.com>
2023-02-26 16:36:00 +0200

    current fuel, range

--- game_objects.py
+++ game_objects.py
@@ -5,7 +5,7 @@ class Airplane:
         self.name = name
         self.fuel_capacity = fuel_capacity
         self.fuel_efficiency = fuel_efficiency
-        # ? current_fuel ?
+        self.current_fuel = 0
 
 
 class Player:
@@ -16,18 +16,20 @@ class Player:
         self.fuel = fuel
         self.location = location
         self.plane = plane # Currently not stored between sessions. TODO Add to database?
-        # ? Max range = 1.0km * fuel (<= plane.fuel_capacity) * plane.fuel_efficiency ?
+        self.range = 1.0 * self.plane.current_fuel * self.plane.fuel_efficiency
     
     def __str__(self):
         return  f'Player: {self.name}, TSLA stocks: {self.money}, Fuel: {self.fuel}, Current location: {self.location}\n' \
-                f'Plane: {self.plane.name}, Fuel Capacity: {self.plane.fuel_capacity}, Fuel Efficiency: {self.plane.fuel_efficiency}'
+                f'Plane: {self.plane.name}, Current Fuel: {self.plane.current_fuel}, Fuel Capacity: {self.plane.fuel_capacity}, Fuel Efficiency: {self.plane.fuel_efficiency}'
 
     def sql_values(self):
         return f'\'{self.id}\', {self.fuel}, {self.money}, {self.location}, \'{self.name}\''
 
 
 #if __name__ == "__main__":
-#    test_player = Player('Player 1', 250, 1000, 'KBOS')
+#    test_player = Player('Player', 'Player 1', 250, 1000, 'KBOS')
 #    test_musk = Player('Elon Musk', 1000000, 9999999, 'KBOI', Airplane('Air Force Musk'))
+#    print(test_player)
+#    print('')
 #    print(test_player.sql_values())
 #    print(test_musk)
