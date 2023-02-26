bc565b993eeb134349b7579239d9526ad2938fd5
Riku <riku.helastera@outlook.com>
2023-02-26 19:30:38 +0200

    sql values update, todos

--- game_objects.py
+++ game_objects.py
@@ -6,6 +6,7 @@ class Airplane:
         self.fuel_capacity = fuel_capacity
         self.fuel_efficiency = fuel_efficiency
         self.current_fuel = 0
+        # TODO self.speed = ?
 
 
 class Player:
@@ -15,15 +16,16 @@ class Player:
         self.money = money
         self.fuel = fuel
         self.location = location
-        self.plane = plane # Currently not stored between sessions. TODO Add to database?
+        self.plane = plane
         self.range = 1.0 * self.plane.current_fuel * self.plane.fuel_efficiency
+        # TODO self.ap = ? (base ap + speed?)
     
     def __str__(self):
         return  f'Player: {self.name}, TSLA stocks: {self.money}, Fuel: {self.fuel}, Current location: {self.location}\n' \
                 f'Plane: {self.plane.name}, Current Fuel: {self.plane.current_fuel}, Fuel Capacity: {self.plane.fuel_capacity}, Fuel Efficiency: {self.plane.fuel_efficiency}'
 
     def sql_values(self):
-        return f'\'{self.id}\', {self.fuel}, {self.money}, {self.location}, \'{self.name}\''
+        return f'\'{self.id}\', {self.fuel}, {self.money}, {self.location}, \'{self.name}\', \'{self.plane.name}\', {self.plane.current_fuel}'
 
 
 #if __name__ == "__main__":
