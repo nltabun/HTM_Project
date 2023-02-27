4aa15df3b46dfd4144bd8978c96764bdab84cd7c
Riku <riku.helastera@outlook.com>
2023-02-27 19:22:21 +0200

    airplane stats func, comments

--- game_objects.py
+++ game_objects.py
@@ -1,26 +1,29 @@
 #
 
 class Airplane:
-    def __init__(self, name, fuel_capacity=10000, fuel_efficiency=1.0):
+    def __init__(self, name, fuel_capacity=10000, fuel_efficiency=1.0, current_fuel=0):
         self.name = name
         self.fuel_capacity = fuel_capacity
-        self.fuel_efficiency = fuel_efficiency
-        self.current_fuel = 0
+        self.fuel_efficiency = fuel_efficiency # lower = better
+        self.current_fuel = current_fuel # int(8)
         # TODO self.speed = ?
+
+    def stats(self): # returns plane statistics
+        return f'\'{self.name}\', {self.fuel_capacity}, {self.fuel_efficiency}, {self.current_fuel}'
 
 
 class Player:
     def __init__(self, id, name, money, fuel, location, plane=Airplane('Default Plane')):
-        self.id = id
-        self.name = name
-        self.money = money
-        self.fuel = fuel
-        self.location = location
-        self.plane = plane
-        self.range = 1.0 * self.plane.current_fuel * self.plane.fuel_efficiency
+        self.id = id # varchar(40)
+        self.name = name # varchar(40)
+        self.money = money # int(8)
+        self.fuel = fuel # int(8)
+        self.location = location # varchar(10)
+        self.plane = plane # varchar(40)
+        self.range = 1.0 * self.plane.current_fuel * self.plane.fuel_efficiency # max single flight distance
         # TODO self.ap = ? (base ap + speed?)
     
-    def __str__(self):
+    def __str__(self): # returns statistics for the player and their plane
         return  f'Player: {self.name}, TSLA stocks: {self.money}, Fuel: {self.fuel}, Current location: {self.location}\n' \
                 f'Plane: {self.plane.name}, Current Fuel: {self.plane.current_fuel}, Fuel Capacity: {self.plane.fuel_capacity}, Fuel Efficiency: {self.plane.fuel_efficiency}'
 
