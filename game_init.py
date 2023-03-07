fc2a41a85f67962193e654bbf9ce168560bdd44e
Riku <riku.helastera@outlook.com>
2023-03-07 16:44:29 +0200

    more planes, better prompt, comment

--- game_init.py
+++ game_init.py
@@ -23,12 +23,18 @@ def generate_airplanes():
     planes = []
 
     plane_musk = game_objects.Airplane('Air Force Musk', 50000, 0.9, 1000, 50000, 999999)
-    plane1 = game_objects.Airplane('Cloudbus A69', 48000, 1.0, 850, 0, 1000)
-    plane2 = game_objects.Airplane('Boijong 420', 100000, 1.1, 900, 0, 1200)
+    plane1 = game_objects.Airplane('MickyD CD-01', 48000, 1.0, 850, 0, 150)
+    plane2 = game_objects.Airplane('Boijong 420', 100000, 1.1, 900, 0, 1050)
+    plane3 = game_objects.Airplane('Cloudbus A69', 88000, 0.9, 1000, 0, 1400)
+    plane4 = game_objects.Airplane('Specific Statics F-61 "Tussling Caracara"', 7000, 0.1, 2500, 0, 3000)
+    plane5 = game_objects.Airplane('Sockweed F-66 "Rapture"', 7500, 0.09, 2800, 0, 3500)
     
     planes.append(plane_musk)
     planes.append(plane1)
     planes.append(plane2)
+    planes.append(plane3)
+    planes.append(plane4)
+    planes.append(plane5)
 
     return planes
 
@@ -41,7 +47,7 @@ def setup_game_table(connection, player_name, start_loc, planes, musk_start_loc)
     musk_plane = planes[0]
 
     while True:
-        game_len = input('Short or long game?: ')
+        game_len = input('Short or long game? (S/L): ')
         if game_len.capitalize() == 'Short' or game_len.capitalize() == 'S':
             turns = random.randint(10, 15)
             break
@@ -61,6 +67,7 @@ def setup_game_table(connection, player_name, start_loc, planes, musk_start_loc)
     return
 
 
+# Resets minigame completion status
 def reset_minigames(connection):
     cur = connection.cursor()
     update = f'UPDATE minigame SET completed = 0'
