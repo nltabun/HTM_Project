04a43658290769b836a2969d1f494b022f2405a5
kkoskivi <elmeri.koskivirta@gmail.com>
2023-04-19 16:34:18 +0300

    Made the airports visible on the map with markers

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 'use strict'
 
-async function asynchronousFunction() {                 // asynchronous function is defined by the async keyword
-    console.log('asynchronous download begins');
+async function fetchData(url) {                 // asynchronous function is defined by the async keyword
     try {                                               // error handling: try/catch/finally
-        const response = await fetch('http://127.0.0.1:5000/airport/coordinates/all');    // starting data download, fetch returns a promise which contains an object of type 'response'
-        const coordinateData = await response.json();          // retrieving the data retrieved from the response object using the json() function
-        console.log(coordinateData);    // log the result to the console
+        const response = await fetch(url);    // starting data download, fetch returns a promise which contains an object of type 'response'
+        return  await response.json();          // retrieving the data retrieved from the response object using the json() function
     } catch (error) {
         console.log(error.message);
     }
 }
 
-asynchronousFunction();
+async function gameSetup() {
+    try {
+        const airports = await fetchData('http://127.0.0.1:5000/airport/coordinates/all');
+        console.log(airports);
 
+        for (let airport of airports) {
+            const marker = L.marker([airport[1], airport[2]]).addTo(map);
+            marker .bindPopup(`<b>${airport[0]}</b>`)
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+gameSetup()
 
 const map = L.map('map', {tap: false});
 L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
